991142c1df6a7d93c03d2925d81cdcfcc2aef8c3
Anthony Millhouse <anthony.millhouse@neoworks.com>
2016-06-24 17:29:51 +0100

    Updated correctly

--- pool-sounds.cpp
+++ pool-sounds.cpp
@@ -3,13 +3,13 @@
 // Sketch works on both Photon and Uno...
 // ------------
 
-#include <time.h>
-#include "Arduino/Arduino.h"
+#include <stdio.h>
+#include "application.h"
 
 #define LOUDNESS_PIN A0
 #define SOUND_PIN D1
 
-time_t startTime = 0;
+int startTime = 0;
 long accumulator = 0;
 long numSamples = 0;
 
@@ -18,18 +18,21 @@ void setup()
 	Serial.begin(9600);
 
 	pinMode(LOUDNESS_PIN, INPUT);
-	startTime = time(NULL);
+	startTime = Time.now();
 }
 
 void loop()
 {
-    time_t currentTime = time(NULL);
+    int currentTime = Time.now();
 
-    if (currentTime - startTime > 30)
+    if (currentTime - startTime > 10)
     {
         /* Calculate and send average. */
         int average = accumulator / numSamples;
 
+        char str[15];
+        sprintf(str, "%d", average);
+        Particle.publish("sound", str, PRIVATE);
 
         /* Reset globals. */
         accumulator = 0;
