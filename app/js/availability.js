dd5296b386842666a8d6c7f9a8656ad0513a662b
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-06-24 17:10:31 +0100

    Added logic to update DOM based on hardcoded availability boolean

--- app/js/availability.js
+++ app/js/availability.js
@@ -3,12 +3,35 @@ pool.availability = (function(){
 
 	// setup
 	var config = {
-		delayDuration: 30000
+		delayDuration: 30000,
+		indicator: {
+			available: "AVAILABLE",
+			unavailable: "NOT available"
+		}
 	};
 
 	// private methods
 	var checkAvailability = function() {
 		console.log('do ajax call');
+
+		// get elements to update and time
+		var availabilityIndicator = document.getElementById('availabilityIndicator');
+		var lastCheck = document.getElementById('lastCheck');
+		var now = new Date().toString();
+
+		// check if available
+		var available = false;
+
+		// update page with availability
+		if (available) {
+			availabilityIndicator.innerHTML = config.indicator.available;
+		} else {
+			availabilityIndicator.innerHTML = config.indicator.unavailable;
+		}
+
+		// update time last checked
+		lastCheck.innerHTML = now;
+
 	};
 
 	// set a delayed check for availability
@@ -31,6 +54,7 @@ pool.availability = (function(){
 		// Add event to check availability on click of button
 		var checkBtn = document.getElementById('checkBtn');
 		checkBtn.addEventListener('click', checkAvailability);
+
 	};
 
 	// public methods
